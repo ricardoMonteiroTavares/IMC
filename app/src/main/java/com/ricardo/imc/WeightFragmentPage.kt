39d6dc6de16d72d829b4638e1d98ceeeef0d169f
Ricardo Monteiro <ricardotavares51@hotmail.com>
2021-03-17 19:55:49 -0300

    Criação do fragmento que busca o peso do usuário

--- app/src/main/java/com/ricardo/imc/WeightFragmentPage.kt
+++ app/src/main/java/com/ricardo/imc/WeightFragmentPage.kt
@@ -8,24 +8,20 @@ import android.view.ViewGroup
 import android.widget.Button
 import androidx.navigation.fragment.findNavController
 
-/**
- * A simple [Fragment] subclass as the second destination in the navigation.
- */
-class SecondFragment : Fragment() {
-
+class WeightFragmentPage: Fragment()  {
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false)
+        return inflater.inflate(R.layout.weight_fragment_page, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         view.findViewById<Button>(R.id.button_second).setOnClickListener {
-            findNavController().navigate(R.id.action_SecondFragment_to_WeightFragmentPage)
+            findNavController().navigate(R.id.action_WeightFragmentPage_to_FirstFragment)
         }
     }
 }
