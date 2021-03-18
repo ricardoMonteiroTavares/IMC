87310ee44ce15ecfe40ef387c9b9a48f776c4660
Ricardo Monteiro <ricardotavares51@hotmail.com>
2021-03-18 19:40:14 -0300

    Criação da página que mostra o resultado do cálculo do IMC

--- app/src/main/java/com/ricardo/imc/ResultPage.kt
+++ app/src/main/java/com/ricardo/imc/ResultPage.kt
@@ -1,27 +1,27 @@
 package com.ricardo.imc
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 
-class WeightFragmentPage: Fragment()  {
+class ResultPage : Fragment() {
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.weight_fragment_page, container, false)
+        return inflater.inflate(R.layout.fragment_result_page, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<Button>(R.id.button_second).setOnClickListener {
-            findNavController().navigate(R.id.action_WeightFragmentPage_to_ResultPage)
+        view.findViewById<Button>(R.id.btn_finish).setOnClickListener {
+            findNavController().navigate(R.id.action_ResultPage_to_FirstFragment)
         }
     }
 }
