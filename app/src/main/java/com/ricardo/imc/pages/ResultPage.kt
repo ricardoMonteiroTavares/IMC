5db5b48e72a52e36ebeaa8adc90056e55bb81054
Ricardo Monteiro <ricardotavares51@hotmail.com>
2021-03-20 00:07:33 -0300

    Mudança de nomenclatura das classes e dos nomes dos arquivos de layout

--- app/src/main/java/com/ricardo/imc/pages/ResultPage.kt
+++ app/src/main/java/com/ricardo/imc/pages/ResultPage.kt
@@ -1,4 +1,4 @@
-package com.ricardo.imc
+package com.ricardo.imc.pages
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.ricardo.imc.R
 
 class ResultPage : Fragment() {
     override fun onCreateView(
--- app/src/main/java/com/ricardo/imc/pages/WeightPage.kt
+++ app/src/main/java/com/ricardo/imc/pages/WeightPage.kt
@@ -1,4 +1,4 @@
-package com.ricardo.imc
+package com.ricardo.imc.pages
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,14 +7,15 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.navigation.fragment.findNavController
+import com.ricardo.imc.R
 
-class WeightFragmentPage: Fragment()  {
+class WeightPage: Fragment()  {
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.weight_fragment_page, container, false)
+        return inflater.inflate(R.layout.fragment_weight_page, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
